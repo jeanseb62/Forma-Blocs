8038342ce8acd41205108a4c502cc1d8950d2048
Jean FREMY <jeanseb.fremy@gmail.com>
2021-09-12 23:24:19 +0200

    Update DashboardController.php

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Block;
+use App\Entity\Formation;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -26,7 +28,14 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        return [
+            MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home'),
+            MenuItem::linkToUrl('Visiter le site', 'fas fa-globe', '/'),
+            MenuItem::subMenu('Formations', 'fas fa-graduation-cap')->setSubItems([
+                MenuItem::linkToCrud('Ajouter une formation', 'fas fa-book-reader', Formation::class),
+                MenuItem::linkToCrud('Ajouter un bloc', 'fa fa-tags', Block::class), 
+                ]),
+
+            ];
     }
 }

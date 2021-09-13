d11839b7dfbdbe980b58371b44287c36c7653fc8
Jean FREMY <jeanseb.fremy@gmail.com>
2021-09-13 15:52:44 +0200

    contact form

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,12 +2,13 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use App\Entity\Contact;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,11 +18,36 @@ class ContactType extends AbstractType
     {
         $builder
            
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email', EmailType::class)
-            ->add('phone')
-            ->add('message')
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('Email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
             ->add('Envoyer', SubmitType::class)
         ;
     }

d82bc3a97556fdc6347ae3265cc55dc4ad6eb369
Jean FREMY <jeanseb.fremy@gmail.com>
2021-09-14 11:27:36 +0200

    formations

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Formation;
+use App\Form\FormationType;
+use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ObjectManager;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default")
      */
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'formations' => $formationRepository->findAll(),
         ]);
     }
+
 }
---
+++ src/Form/FormationType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('type')
+            ->add('numberBlocks')
+            ->add('listing')
+            ->add('price')
+            ->add('documentPDF')
+            ->add('image')
+            ->add('isPublished')
+            ->add('content')
+            ->add('blocks')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
